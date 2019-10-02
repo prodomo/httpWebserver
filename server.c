58fb7c6a27eef46711c5f56b71685a25629153ab
prodomo <nprodomo@gmail.com>
2019-10-02 09:45:48 +0000

    add send msg but no header yet

--- server.c
+++ server.c
@@ -11,7 +11,7 @@ int main(int argc , char *argv[])
     int socketR = 0, clientR;
     int backlogNum =5;
     char inputBuffer[256]={};
-    char message[]={"Hi,this is server.\n"};
+    char message[]={"HTTP/1.1 200 OK!\n\r"};
     struct sockaddr_in server_info, client_info;
     int addrlen = sizeof(client_info);
 
@@ -36,6 +36,9 @@ int main(int argc , char *argv[])
         clientR = accept(socketR, (struct sockaddr*)&client_info, &addrlen);
         recv(clientR, inputBuffer,sizeof(inputBuffer),0);
         printf("Get:%s\n",inputBuffer);
+        send(clientR, message, sizeof(message), 0);
+        printf("send:%s\n",message);
+        close(clientR);
     }
 
     return 0;
